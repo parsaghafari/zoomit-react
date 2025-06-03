52ecbb7feac60c5131e04989851e31c446812b01
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-03 18:24:33 +0330

    Add route for home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Fragment } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Fragment>
+  );
 }
 
 export default App;
