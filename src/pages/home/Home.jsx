4a2a394505bda61d9c8bf063366786e218449712
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-03 23:05:54 +0330

    Add header to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import TopBanner from '../../components/top-banner/TopBanner';
+import Header from '../../components/header/Header';
 
 function Home() {
   return (
     <Fragment>
       <TopBanner />
+      <Header />
     </Fragment>
   );
 }
