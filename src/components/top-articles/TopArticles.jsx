45832fab5350836e3c1b33e64a017039dd6cc3f9
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-08 20:29:44 +0330

    Add initial styles to the TopArticles component

--- src/components/top-articles/TopArticles.jsx
+++ src/components/top-articles/TopArticles.jsx
@@ -1,21 +1,22 @@
 import iranNetImg from '../../assets/images/top-articles/iran-internet.png';
 import momotaroImg from '../../assets/images/top-articles/MOMOTARO_JEANS_AOYAMA.png';
 import ps5Img from '../../assets/images/top-articles/ps5pro3.png';
+import styles from './TopArticles.module.css';
 
 function TopArticles() {
   return (
-    <div className="top-articles">
+    <div className={styles.top_articles}>
       <a href="#" className="article-1">
         <img src={momotaroImg} alt="momotaro jeans store" />
-        <h2 className="article-title">موموتارو جینز</h2>
+        <h4 className={styles.article_title}>موموتارو جینز</h4>
       </a>
       <a href="#" className="article-2">
         <img src={iranNetImg} alt="iran internet" />
-        <h2 className="article-title">اینترنت ایران</h2>
+        <h4 className={styles.article_title}>اینترنت ایران</h4>
       </a>
       <a href="#" className="article-3">
         <img src={ps5Img} alt="ps5 pro" />
-        <h2 className="article-title">پلی استیشن 5 پرو</h2>
+        <h4 className={styles.article_title}>پلی استیشن 5 پرو</h4>
       </a>
     </div>
   );
