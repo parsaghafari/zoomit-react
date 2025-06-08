3593bd6a3b12b28269a2361d4936e52373652cab
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-08 15:17:24 +0330

    Import top articles images

--- src/components/top-articles/TopArticles.jsx
+++ src/components/top-articles/TopArticles.jsx
@@ -1,19 +1,20 @@
+import iranNetImg from '../../assets/images/top-articles/iran-internet.png';
+import momotaroImg from '../../assets/images/top-articles/MOMOTARO_JEANS_AOYAMA.png';
+import ps5Img from '../../assets/images/top-articles/ps5pro3.png';
+
 function TopArticles() {
   return (
     <div className="top-articles">
       <a href="#" className="article-1">
-        <img
-          src="assets/images/blog/MOMOTARO_JEANS_AOYAMA.png"
-          alt="momotaro jeans store"
-        />
+        <img src={momotaroImg} alt="momotaro jeans store" />
         <h2 className="article-title">موموتارو جینز</h2>
       </a>
       <a href="#" className="article-2">
-        <img src="assets/images/blog/iran-internet.png" alt="iran internet" />
+        <img src={iranNetImg} alt="iran internet" />
         <h2 className="article-title">اینترنت ایران</h2>
       </a>
       <a href="#" className="article-3">
-        <img src="assets/images/blog/ps5pro3.png" alt="ps5 pro" />
+        <img src={ps5Img} alt="ps5 pro" />
         <h2 className="article-title">پلی استیشن 5 پرو</h2>
       </a>
     </div>
