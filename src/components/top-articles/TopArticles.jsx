72b351d9048296a71d61c070fedddad15076b33c
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-08 15:12:32 +0330

    Create a component and initial jsx

---
+++ src/components/top-articles/TopArticles.jsx
@@ -0,0 +1,23 @@
+function TopArticles() {
+  return (
+    <div className="top-articles">
+      <a href="#" className="article-1">
+        <img
+          src="assets/images/blog/MOMOTARO_JEANS_AOYAMA.png"
+          alt="momotaro jeans store"
+        />
+        <h2 className="article-title">موموتارو جینز</h2>
+      </a>
+      <a href="#" className="article-2">
+        <img src="assets/images/blog/iran-internet.png" alt="iran internet" />
+        <h2 className="article-title">اینترنت ایران</h2>
+      </a>
+      <a href="#" className="article-3">
+        <img src="assets/images/blog/ps5pro3.png" alt="ps5 pro" />
+        <h2 className="article-title">پلی استیشن 5 پرو</h2>
+      </a>
+    </div>
+  );
+}
+
+export default TopArticles;
