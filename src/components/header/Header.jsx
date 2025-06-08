04153d75909f92acb0416c1841ce97d814d90099
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-08 15:33:53 +0330

    Add TopArticles component to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../navbar/Navbar';
+import TopArticles from '../top-articles/TopArticles';
 import styles from './Header.module.css';
 
 function Header() {
   return (
     <header>
       <Navbar />
+      <TopArticles />
     </header>
   );
 }
