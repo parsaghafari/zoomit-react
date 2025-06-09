0b5fd4f8d019786a08f2e674e8f48729f6855e26
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-09 20:36:18 +0330

    Add swiper for secondary nav items

--- src/components/secondary-nav/SecondaryNav.jsx
+++ src/components/secondary-nav/SecondaryNav.jsx
@@ -1,6 +1,9 @@
 import styles from './SecondaryNav.module.css';
-import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import { Navigation, Scrollbar } from 'swiper/modules';
 
 function SecondaryNav() {
   return (
@@ -15,10 +18,174 @@ function SecondaryNav() {
           </a>
         </div>
         <div className={styles.left_side}>
-          <button className="scroll-right">
+          <Swiper
+            scrollbar={{
+              hide: true,
+            }}
+            navigation={true}
+            modules={[Navigation, Scrollbar]}
+            className="mySwiper"
+            slidesPerView={'7'}
+            spaceBetween={30}
+          >
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/fluency-systems-regular/100/ffffff/iphone.png"
+                  alt="iphone"
+                />
+                <span>گوشی</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/windows/100/ffffff/ipad.png"
+                  alt="ipad"
+                />
+                <span>تبلت</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/windows/100/ffffff/laptop.png"
+                  alt="laptop"
+                />
+                <span>لپ تاپ</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/windows/100/ffffff/tv.png"
+                  alt="tv"
+                />
+                <span>تلویزیون</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/ios/100/ffffff/apple-watch.png"
+                  alt="apple-watch"
+                />
+                <span>ساعت هوشمند</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/windows/100/ffffff/headphones.png"
+                  alt="headphones"
+                />
+                <span>هدفون</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/dotty/100/ffffff/hdd.png"
+                  alt="hdd"
+                />
+                <span>هارد</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/parakeet-line/100/ffffff/controller.png"
+                  alt="gamepad"
+                />
+                <span>کنسول بازی</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/wired/100/ffffff/video-card.png"
+                  alt="video-card"
+                />
+                <span>کارت گرافیک</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/material-outlined/100/ffffff/smartphone-cpu.png"
+                  alt="cpu"
+                />
+                <span>پردازنده</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/material-outlined/100/ffffff/monitor.png"
+                  alt="monitor"
+                />
+                <span>مانیتور</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/parakeet-line/100/ffffff/ssd.png"
+                  alt="ssd"
+                />
+                <span>SSD</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/windows/100/ffffff/camera--v1.png"
+                  alt="camera"
+                />
+                <span>دوربین</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/external-nawicon-detailed-outline-nawicon/100/ffffff/external-power-bank-energy-nawicon-detailed-outline-nawicon.png"
+                  alt="external power bank"
+                />
+                <span>پاوربانک</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <a href="#">
+                <img
+                  width="64"
+                  height="64"
+                  src="https://img.icons8.com/external-xnimrodx-lineal-xnimrodx/100/ffffff/external-charger-electronics-xnimrodx-lineal-xnimrodx.png"
+                  alt="charger"
+                />
+                <span>شارژر</span>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://img.icons8.com/carbon-copy/100/ffffff/speaker/subwoofer.png"
+                  alt="speaker"
+                />
+                <span>اسپیکر</span>
+              </a>
+            </SwiperSlide>
+          </Swiper>
+          {/* <button className="scroll-right">
             <i className="fa-solid fa-angle-right"></i>
-          </button>
-          <nav>
+          </button> */}
+          {/* <nav>
             <ul className="nav-list">
               <li className="nav-item">
                 <a href="#">
@@ -167,10 +334,10 @@ function SecondaryNav() {
                 </a>
               </li>
             </ul>
-          </nav>
-          <button className="scroll-left">
+          </nav> */}
+          {/* <button className="scroll-left">
             <i className="fa-solid fa-angle-left"></i>
-          </button>
+          </button> */}
         </div>
       </div>
     </section>
