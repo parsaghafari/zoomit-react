0f4e337ca8ae3780f63061d9e3ddfbd42aaf52d0
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-12 23:02:09 +0330

    Add another advertisement component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,8 @@ import SecondaryNav from '../../components/secondary-nav/SecondaryNav';
 import Ads from '../../components/ads/Ads';
 import ad1 from '../../assets/images/ads/shatel-ad.gif';
 import ad2 from '../../assets/images/ads/arizzo-ad.gif';
+import ad3 from '../../assets/images/rodeo-ad.gif';
+import ad4 from '../../assets/images/systan-ad.gif';
 import EditorsChoice from '../../components/editors-choice/EditorsChoice';
 import LatestVideos from '../../components/latest-videos/LatestVideos';
 
@@ -28,6 +30,12 @@ function Home() {
           <div className="section-container">
             <div className="right-side">
               <LatestVideos />
+              <Ads
+                firstSrc={ad3}
+                firstAlt={'rodeo exchange advertisement'}
+                secondSrc={ad4}
+                secondAlt={'systan.com advertisement'}
+              />
             </div>
             <div className="left-side"></div>
           </div>
