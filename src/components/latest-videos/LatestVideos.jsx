f28eb2505a1c0a78bd526da4811e0db0cf82cf69
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-12 17:29:12 +0330

    Implement swiper slider in the latest videos

--- src/components/latest-videos/LatestVideos.jsx
+++ src/components/latest-videos/LatestVideos.jsx
@@ -1,3 +1,8 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import { Navigation, Scrollbar } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './LatestVideos.module.css';
 
 function LatestVideos() {
@@ -10,6 +15,16 @@ function LatestVideos() {
           <i className="fa-solid fa-arrow-left"></i>
         </a>
       </div>
+      <Swiper
+        scrollbar={{
+          hide: true,
+        }}
+        navigation={true}
+        modules={[Navigation, Scrollbar]}
+        className={styles.swiper}
+      >
+        <SwiperSlide />
+      </Swiper>
     </div>
   );
 }
