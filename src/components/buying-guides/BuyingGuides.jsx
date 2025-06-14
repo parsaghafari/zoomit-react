5ff91c44eebdf23f0a228ff1d052b430d3ab7c31
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-14 23:18:04 +0330

    Create a component and add its initial jsx

---
+++ src/components/buying-guides/BuyingGuides.jsx
@@ -0,0 +1,69 @@
+import styles from './BuyingGuides.module.css';
+
+function BuyingGuides() {
+  return (
+    <div className={styles.buying_guides}>
+      <div className={styles.head}>
+        <h3>راهنمای خرید</h3>
+        <a href="#" className={styles.view_all}>
+          <span>مشاهده همه</span>
+          <i className="fa-solid fa-arrow-left"></i>
+        </a>
+      </div>
+      <div className="slider-container">
+        <button className="scroll-right">
+          <i className="fa-solid fa-angle-right"></i>
+        </button>
+        <div className="slider">
+          <a href="#" className="slider-item">
+            <h4>
+              <i className="fa-solid fa-fire"></i>
+              بهترین دسته بازی برای کامپیوتر [دی 1403]
+            </h4>
+            <img src="assets/images/blog/gamepads.jpg" alt="some gamepads" />
+            <div className="time-date">
+              <span>
+                <i className="fa-regular fa-clock"></i> 10&prime; مطالعه
+              </span>
+              <span>
+                <i className="fa-regular fa-calendar"></i>2 روز پیش
+              </span>
+            </div>
+          </a>
+          <a href="#" className="slider-item">
+            <h4>بهترین مسواک برقی بازار [دی 1403]</h4>
+            <img
+              src="assets/images/blog/toothbrush.jpg"
+              alt="electronic toothbrushes"
+            />
+            <div className="time-date">
+              <span>
+                <i className="fa-regular fa-clock"></i> 5&prime; مطالعه
+              </span>
+              <span>
+                <i className="fa-regular fa-calendar"></i>1 روز پیش
+              </span>
+            </div>
+          </a>
+          <a href="#" className="slider-item">
+            <h4>بهترین پردازنده [دی 1403]</h4>
+            <img src="assets/images/blog/cpus.jpg" alt="some cpus" />
+            <div className="time-date">
+              <span>
+                <i className="fa-regular fa-clock"></i> 10&prime; مطالعه
+              </span>
+              <span>
+                <i className="fa-regular fa-calendar"></i>3 روز پیش
+              </span>
+            </div>
+          </a>
+        </div>
+        <button className="scroll-left">
+          <i className="fa-solid fa-angle-left"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BuyingGuides;
