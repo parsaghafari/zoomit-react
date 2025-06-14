928fec49e5ed843069e40d384b2fc2d1e844d302
Parsa Ghafari <parsa.ghafari2@gmail.com>
2025-06-14 21:53:07 +0330

    Add another advertisement

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,8 @@ import ad1 from '../../assets/images/ads/shatel-ad.gif';
 import ad2 from '../../assets/images/ads/arizzo-ad.gif';
 import ad3 from '../../assets/images/rodeo-ad.gif';
 import ad4 from '../../assets/images/systan-ad.gif';
+import ad5 from '../../assets/images/fly-today-ad.gif';
+import ad6 from '../../assets/images/zoomit-meetup-ad.gif';
 import EditorsChoice from '../../components/editors-choice/EditorsChoice';
 import LatestVideos from '../../components/latest-videos/LatestVideos';
 import MostVisited from '../../components/most-visited/MostVisited';
@@ -38,6 +40,12 @@ function Home() {
                 secondAlt={'systan.com advertisement'}
               />
               <MostVisited />
+              <Ads
+                firstSrc={ad5}
+                firstAlt={'fly today advertisement'}
+                secondSrc={ad6}
+                secondAlt={'zoomit meetup advertisement'}
+              />
             </div>
             <div className="left-side"></div>
           </div>
